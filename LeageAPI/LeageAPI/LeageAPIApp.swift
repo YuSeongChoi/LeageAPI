078aa48bd65ebc2c07dd66dcb048849768a16246
cys <cys@iquest.kr>
2022-11-16 14:49:21 +0900

    init project

---
+++ LeageAPI/LeageAPI/LeageAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeageAPIApp.swift
+//  LeageAPI
+//
+//  Created by YuSeongChoi on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct LeageAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
